dc72db8f1bfbd9440da5c163659e39d064e79670
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2023-07-27 17:22:55 -0600

    27

---
+++ constants/styles.js
@@ -0,0 +1,29 @@
+export const GlobalStyles = {
+  colors: {
+    error100: '#FD989D',
+    error500: '#9B091A',
+    flare: '#bada55',
+  },
+  light: {
+    text: '#050505',
+    background: '#fafafa',
+    primary100: '#D3F1DA',
+    primary300: '#9DDFAD',
+    primary500: '#7ad180',
+    primary700: '#559263',
+    primary900: '#315439',
+    secondary: '#d9f2db',
+    accent: '#19e627',
+  },
+  dark: {
+    text: '#fafafa',
+    background: '#050505',
+    primary900: '#D3F1DA',
+    primary700: '#9DDFAD',
+    primary500: '#7ad180',
+    primary300: '#559263',
+    primary100: '#315439',
+    secondary: '#0d260f',
+    accent: '#75f07d',
+  }
+}
